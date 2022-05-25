6b847740b68962affd9a28fb0ee0f6791ca18192
unknown <afir4u@gmail.com>
2022-05-25 18:27:12 +0530

    added reports

---
+++ HTML_20Report/src/test/java/org/tng/BaseClass.java
@@ -0,0 +1,67 @@
+package org.tng;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+import com.paulhammant.ngwebdriver.NgWebDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	static WebDriver driver;
+	static NgWebDriver ngdriver;
+	public static void launchBrowser(String browser) {
+		if (browser.equals("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+			
+		}
+		else if (browser.equals("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		} else {
+	        WebDriverManager.iedriver().setup();
+	        driver=new InternetExplorerDriver();
+		}
+	}
+	public static void launchUrl(String url) {
+		
+		
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		String title = driver.getTitle();
+		System.out.println(title);
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+		
+	
+
+	}
+	
+	
+
+public static void clickBtn(WebElement e) {
+	e.click();
+
+}
+	public static void searchSong(WebElement e,String value) {
+		e.sendKeys(value);
+
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
